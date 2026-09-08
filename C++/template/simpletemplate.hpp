8ed5351bab9d22882f294c0b002fbdff3c8769c0
LiuZhaoyang <1343183241@qq.com>
2026-09-08 13:17:17 +0800

    加了点注释

--- C++/template/simpletemplate.hpp
+++ C++/template/simpletemplate.hpp
@@ -1,3 +1,7 @@
+/**
+ * @file simpletemplate.hpp
+ * @brief 简单的单参数模板函数示例
+ */
 template <typename T>
 T add(T a, T b)
 {
---
+++ C++/template/template_parameter_pack.hpp
@@ -0,0 +1,9 @@
+/**
+ * @file template_parameter_pack.hpp
+ * @brief 模板参数包示例
+ */
+
+template <typename... Args>
+void print(Args... args)
+{
+}
